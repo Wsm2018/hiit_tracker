3b727de89de8f6202ba75542a3b3e39ee43cf1b5
Wsm2018 <40735297+Wsm2018@users.noreply.github.com>
2021-10-25 03:03:57 +0300

    Added Comparative analysis
    - Completed a comparative analysis of a similar application that is used for HIIT tracking performance

--- Comparison.js
+++ Comparison.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Button, View, TouchableOpacity, Text, ScrollView } from "react-native";
+import { Button, View, TouchableOpacity, Text, Image } from "react-native";
 import { Header } from "react-native-elements";
 import { Icon } from "react-native-elements";
 
@@ -21,6 +21,31 @@ export default function Comparison({ navigation }) {
           style: { color: "#000", fontWeight: "bold", fontSize: 20 },
         }}
       />
+      <View style={{ margin: "5%" }}>
+        <Text style={{ fontWeight: "bold", fontSize: 30 }}>Comparison</Text>
+      </View>
+
+      <View style={{ marginTop: "1%", marginLeft: "3%" }}>
+        <Text style={{ fontSize: 18 }}>
+          This contains a comparison analysis between similar application and
+          overviwing the features between the applications and the differences.
+          This is to understand what similar applications offers users and how
+          it helps them.
+        </Text>
+      </View>
+
+      <View style={{ marginTop: "5%", marginLeft: "3%", alignItems: "center" }}>
+        <Image source={require("./assets/comparison.png")} />
+      </View>
+
+      <View style={{ marginTop: "5%", marginLeft: "3%", alignItems: "center" }}>
+        <Text style={{ fontSize: 18 }}>
+          Seconds Pro, is a mobile fitness application that is used by users to track their HIIT workout performance
+          The application works with wearables devices which is an extremely plus point since most people in this time
+          wears or has wearable devices such as Apple Watch, Samsung Galaxy watch, Fitbits, etc. 
+
+        </Text>
+      </View>
     </View>
   );
 }
